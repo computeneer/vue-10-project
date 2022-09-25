55f08cde53ee30f2ffeb035865928878e484841e
Computeneer <computeneer@gmail.com>
2022-09-25 23:44:11 +0300

    store enhancement

--- app-quiz/src/store/index.js
+++ app-quiz/src/store/index.js
@@ -1,13 +1,21 @@
 import { createStore } from "vuex";
-
+import axios from "@/utils/axios";
 const store = createStore({
   state: {
     isAuthenticated: false,
     user: null,
+    categories: [],
+    quizzes: [],
   },
   getters: {
     getIsAuthenticated(state) {
       return state.isAuthenticated;
+    },
+    getCategories(state) {
+      return state.categories;
+    },
+    getQuizzes(state) {
+      return state.quizzes;
     },
   },
   mutations: {
@@ -19,8 +27,31 @@ const store = createStore({
       state.isAuthenticated = false;
       state.user = null;
     },
+    setCategories(state, payload) {
+      state.categories = payload;
+    },
+    setQuizzes(state, payload) {
+      state.quizzes = payload;
+    },
   },
-  actions: {},
+  actions: {
+    async login({ commit }, formData) {
+      const { data } = await axios.get(`/users?username=${formData.username}&password=${formData.password}`);
+      if (data.length > 0) {
+        commit("login", data);
+        return true;
+      }
+      return false;
+    },
+    async fetchDatas({ commit }) {
+      axios.get("categories").then((category_response) => {
+        commit("setCategories", category_response.data);
+      });
+      axios.get("quizzes").then((quizzes_response) => {
+        commit("setQuizzes", quizzes_response.data);
+      });
+    },
+  },
 });
 
 export default store;
